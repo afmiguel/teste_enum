d3f270bfaf73db741e287f7da6299359bec2bd2c
afmiguel <57543621+afmiguel@users.noreply.github.com>
2023-10-04 09:41:15 -0300

    Inserido código de exemplo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+enum EstadoAnimal {
+    Vivo,
+    Extinto,
+    Desconhecido,
+}
+
+fn imprimir_estado(estado: EstadoAnimal) {
+    if let EstadoAnimal::Vivo = estado {
+        println!("O animal está vivo");
+    } else if let EstadoAnimal::Extinto = estado {
+        println!("O animal está extinto");
+    } else if let EstadoAnimal::Desconhecido = estado {
+        println!("O estado do animal é desconhecido");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let estado1 = EstadoAnimal::Vivo;
+    let estado2 = EstadoAnimal::Extinto;
+    let estado3 = EstadoAnimal::Desconhecido;
+    
+    imprimir_estado(estado1);
+    imprimir_estado(estado2);
+    imprimir_estado(estado3);
 }
